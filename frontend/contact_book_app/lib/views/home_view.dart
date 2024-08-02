ea9301a11507de5c6247459a426ea5fe6b7f3702
beatrizgomees <beatrizgomesxx@gmail.com>
2024-08-02 16:15:28 -0300

    organized the code

--- frontend/contact_book_app/lib/views/home_view.dart
+++ frontend/contact_book_app/lib/views/home_view.dart
@@ -25,14 +25,30 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
+    return Scaffold(
+      
+      appBar: _buildTopSectionHomeView(context),
+      floatingActionButton: _buildFloatButtonCreateContact(context),
+    
+      backgroundColor: AppTheme.backgroundPrincipalColor,
+      body: Consumer<HomeViewModel>(
+        builder: (context, viewModel, _) {
+        return SingleChildScrollView(
+          child: _buildListContact(context, viewModel)
+                );
+              }
+            ),   
+          );
+        }
+      }
+
+_buildTopSectionHomeView(BuildContext context){
+  return AppBar(
           leadingWidth: 100,
           backgroundColor: AppTheme.backgroundPrincipalColor,
           actions: [
           IconButton(
-            icon: Icon(Icons.notifications), 
+            icon: const Icon(Icons.notifications), 
             color: Colors.white,
              onPressed: () {
               Navigator.pushNamed(context, '/notifications');
@@ -43,8 +59,12 @@ class _HomeViewState extends State<HomeView> {
           "Contacts", 
           style: AppTheme.darkFontStyleContactName,
           ),
-        ),
-        floatingActionButton:  FloatingActionButton.extended(onPressed: () {
+        );
+}
+
+_buildFloatButtonCreateContact(BuildContext context){
+   return FloatingActionButton.extended(
+          onPressed: () {
           Navigator.of(context).push(
             MaterialPageRoute(
               builder: (context) =>  const CreateContactView(),
@@ -54,53 +74,45 @@ class _HomeViewState extends State<HomeView> {
             style: AppTheme.darkFontStyleContatcSubtitle),
             backgroundColor: Colors.blue.shade700,
             icon: const Icon(Icons.add_call, color: Colors.white),
-            ),
-        backgroundColor: AppTheme.backgroundPrincipalColor,
-        body: Consumer<HomeViewModel>(
-          builder: (context, viewModel, _) {
-          return SingleChildScrollView(
-            child: FutureBuilder(
-            future: Provider.of<HomeViewModel>(context, listen: false).fetchContacts(),
-              builder: (context, snapshot) {
-                   if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
             );
-          } else if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.hasError) {
-              // Handle errors
-              return Center(
-                child: Text('Error: ${snapshot.error}'),
-              );
-            }
-             return ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-                itemCount: viewModel.contacts.length,
-                itemBuilder: (context, index) {
-                   var contact = viewModel.contacts[index];
-                      return CardContactComponent(
-                        email: contact.email!, 
-                        name: contact.name!,
-                        );
-                          },
-                        );
-                      }
-                      else{
-                         return const Center(
-                            child: Text('Something went wrong!'),
-                         );
-                      }
-              }
-                    )
-                  );
-                }
-              ),   
-              
-               
-                )
-              );
-            }
-          }
+}
 
+Widget _getData(var snapshot){
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return const Center(
+      child: CircularProgressIndicator(),
+    );
+    } else if (snapshot.connectionState == ConnectionState.done) {
+      if (snapshot.hasError) {
 
+    // Handle errors
+      return Center(
+      child: Text('Error: ${snapshot.error}'),
+    );
+    }
+  }
+  return const Center(
+    child: Text('Something went wrong!'),
+  );
+}
+
+_buildListContact(BuildContext context, HomeViewModel viewModel){
+  return FutureBuilder(
+  future: Provider.of<HomeViewModel>(context, listen: false).fetchContacts(),
+  builder: (context, snapshot) {
+  _getData(snapshot);
+  return ListView.builder(
+  shrinkWrap: true,
+  physics: const NeverScrollableScrollPhysics(),
+  itemCount: viewModel.contacts.length,
+  itemBuilder: (context, index) {
+    var contact = viewModel.contacts[index];
+      return CardContactComponent(
+        email: contact.email!, 
+        name: contact.name!,
+        );
+          },
+        );
+      }
+    );
+}
