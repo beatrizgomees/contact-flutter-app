79cfe6fa2b61087854556db667fad5381472b925
beatrizgomees <beatrizgomesxx@gmail.com>
2024-09-07 17:19:45 -0300

    build profile view and edit view

---
+++ frontend/contact_book_app/lib/features/contact_crud/edit_list_contact_view.dart
@@ -0,0 +1,53 @@
+import 'package:contact_book_app/features/contact_crud/edit_list_contact_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class EditListContactView extends StatefulWidget {
+  const EditListContactView({super.key});
+
+  @override
+  State<EditListContactView> createState() => _EditListContactViewState();
+}
+
+class _EditListContactViewState extends State<EditListContactView>  {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Consumer<EditListContactViewModel>(
+          builder: (context, viewModel, child) {
+          return FutureBuilder(
+            future: Provider.of<EditListContactViewModel>(context, listen: false).fetchContacts(),
+            builder: (context, snapshot) {
+              return SizedBox(
+                height: 300,
+                child: ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: viewModel.contacts.length,
+                itemBuilder: (context, index) {
+                var contact = viewModel.contacts[index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: Row(
+                      children: [
+                        const CircleAvatar(),
+                        const SizedBox(width: 20),
+                        Text(contact.name!, style: const TextStyle(fontSize: 20),),
+                      ],
+                    ),),
+                );
+                }
+                ),
+              );
+          }
+          );
+          }
+        ),
+      ),
+    );
+  }
+}
--- frontend/contact_book_app/lib/features/profile/ProfilePage.dart
+++ frontend/contact_book_app/lib/features/profile/ProfilePage.dart
@@ -10,6 +10,109 @@ class Profilepage extends StatefulWidget {
 class _ProfilepageState extends State<Profilepage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+     body: Center(
+       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                   const CircleAvatar(
+                  radius: 50,
+                
+                ),
+                 Positioned(
+                right: 125, // Alinhado à direita
+                top: 10,
+                child: Container(
+                  width: 50,
+                  decoration: BoxDecoration(color: Colors.green.shade300, borderRadius: BorderRadius.circular(20)),
+                  child: const Center(child: Text("Online", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w600,),)))
+                 ),
+                Positioned(
+                right: 150, 
+                bottom: 0,
+                child: IconButton(
+                  icon: const Icon(Icons.edit), // Ícone que você deseja usar
+                  onPressed: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Wrap(
+                        children: [
+                          ListTile(
+                              title: const Text("Camera"),
+                              leading: const Icon(Icons.camera),
+                              onTap: () async {
+                              //viewModel.takeImage(null);
+                              },),
+                          ListTile(
+                            title: const Text("Galery"),
+                            leading: const Icon(Icons.photo),
+                            onTap: () async {
+                            },
+                          )
+                        ],
+                      );
+                    },
+                  );
+                  },
+                ),
+                ),
+                ]
+              ),
+            ),
+          const SizedBox(height: 20),
+
+          Text("Beatriz Gomes", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),),
+          Text("81991664621", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),),
+          Text("beatrizgomesxx@gmail.com", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),),
+        
+          Padding(
+            padding: const EdgeInsets.only(left: 30, top: 30, right: 30),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(50), color: Colors.amber,),
+                  child: IconButton(onPressed:  () { }, icon: Icon(Icons.phone),),
+                ),
+                SizedBox(width: 20),
+                  Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(50), color: Colors.amber,),
+                  child: IconButton(onPressed:  () { }, icon: Icon(Icons.video_camera_front_rounded),),
+                ),
+                  SizedBox(width: 20),
+                  Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(50), color: Colors.amber,),
+                  child: IconButton(onPressed:  () { }, icon: Icon(Icons.chat_bubble),),
+                ),
+                  SizedBox(width: 20),
+                  Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(50), color: Colors.amber,),
+                  child: IconButton(onPressed:  () { }, icon: Icon(Icons.email),),
+                )
+              ],
+            ),
+          ),
+             
+          ],
+        ),
+     ),
+    );
   }
 }
